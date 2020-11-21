032bb739e6ec44721c6e90db4e1128313cb7e2d7
Andres David <dandavi2310@hotmail.com>
2020-11-20 23:59:22 -0500

    avances proyecto

---
+++ src/components/Prueba.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ImageUpload from './ImageUpload';
+
+const Prueba = () => {
+     return (
+          <ImageUpload /> 
+     );
+};
+
+export default Prueba;
+
+
+
+
+
+
+
+
